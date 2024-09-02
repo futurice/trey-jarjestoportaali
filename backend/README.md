ed1689bdd58b8c4362225ab9d471d6b46740f138
Valtteri Valtanen <valtteri.valtanen@futurice.com>
2024-09-02 13:38:08 +0300

    feat: init azure functions

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+# TREY Järjestöportaali backend
+
+Azure Functions backend.
+
+# Prerequisites
+
+- [azure-functions-core-tools@4](https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=macos%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-typescript)
+
+# Getting started
+
+```bash
+$ npm install
+$ npm start
+```
---
+++ backend/src/functions/health.ts
@@ -0,0 +1,25 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+export async function health(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get("name") || (await request.text()) || "world";
+
+  context.info("OK");
+
+  return { body: `Hello, ${name}!` };
+}
+
+app.http("health", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: health,
+});
