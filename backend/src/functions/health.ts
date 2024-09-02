df17b6e964cb28aeb18b6ea3d90da8d0bdedfcfc
Valtteri Valtanen <valtteri.valtanen@futurice.com>
2024-09-02 16:50:16 +0300

    chore: add initial eslint, prettier and ts configs

--- backend/src/functions/health.ts
+++ backend/src/functions/health.ts
@@ -1,25 +1,17 @@
-import {
-  app,
-  HttpRequest,
-  HttpResponseInit,
-  InvocationContext,
-} from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions"
 
-export async function health(
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+export async function health(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`)
 
-  const name = request.query.get("name") || (await request.text()) || "world";
+  const name = request.query.get("name") || (await request.text()) || "world"
 
-  context.info("OK");
+  context.info("OK")
 
-  return { body: `Hello, ${name}!` };
+  return { body: `Hello, ${name}!` }
 }
 
 app.http("health", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: health,
-});
+})
