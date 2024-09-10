36e7468e5828cf6fb829b4ed9d1042f50aa9bac1
Kiti Suupohja <kiti.suupohja@futurice.com>
2024-09-10 17:53:08 +0300

    Add Radix-ui theming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
+import { Container } from "@radix-ui/themes"
 import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Container align="center">
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
@@ -14,7 +15,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </Container>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import { Theme } from "@radix-ui/themes"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Theme accentColor="blue" hasBackground={false} panelBackground="solid">
+      <App />
+    </Theme>
+  </StrictMode>
 )
