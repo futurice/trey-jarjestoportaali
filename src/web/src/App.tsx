30e0a4e79d1691132260e03a1564d4a4f290056d
heidi <heidi.poels@gmail.com>
2025-02-23 15:57:32 +0200

    Fix/missing registration page (#14)
    * fix: added missing registration page
    
    * fix: index points to login

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -5,12 +5,14 @@ import { Layout } from "./components/Layout/Layout";
 import Login from "./components/Login";
 import MyFiles from "./components/MyFiles";
 import { Authenticated, Roles } from "./authentication";
+import {Registration} from "./components/Registration/Registration.tsx";
 
 const approvedRoles = [Roles.ORGANISATION, Roles.TREY_BOARD, Roles.ADMIN];
 
 const App = ()=> {
     return <BrowserRouter>
             <Routes>
+
                 <Route path="/dashboard" element={<Authenticated requiredRoles={approvedRoles} redirectUrl={"/registration"}>
                         <Layout>
                             <Dashboard/>
@@ -24,6 +26,15 @@ const App = ()=> {
                     </Authenticated>}/>
 
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/" element={<Login/>}/>
+
+                <Route path="registration" element={
+                    <Authenticated requiredRoles={[Roles.NONE]} redirectUrl={"/registration"} >
+                        <Layout>
+                            <Registration />
+                        </Layout>
+                    </Authenticated>}>
+                </Route>
             </Routes>
     </BrowserRouter>
 }
