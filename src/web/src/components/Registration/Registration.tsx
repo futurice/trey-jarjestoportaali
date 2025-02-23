21f44d4487a0b53f076f5e9ea3c8019daaf1c8d8
heitzuli <heidi.poels@gmail.com>
2025-02-23 16:30:49 +0200

    feat: add surveyjs form

--- src/web/src/components/Registration/Registration.tsx
+++ src/web/src/components/Registration/Registration.tsx
@@ -1,6 +1,32 @@
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+
+const json = {
+    "logoPosition": "right",
+        "pages": [
+        {
+            "name": "page1",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "question1",
+                    "title": "Name"
+                },
+                {
+                    "type": "text",
+                    "name": "question2",
+                    "title": "Järjestön nimi"
+                }
+            ]
+        }
+    ]
+};
+
 export const Registration = () => {
+    const survey = new Model(json);
     return <div>
         <h1>Registration</h1>
-        <p>Register here</p>
+        <Survey model={survey} />
     </div>
 }
