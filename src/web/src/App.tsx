b90e0358c776f5959b650284c9e8b41db4b38ef9
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-10 14:59:55 +0200

    fix: Refresh session every 25 minutes to prevent logout (#12)

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,42 +1,58 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Dashboard from "./components/Dashboard";
-import { Layout } from "./components/Layout/Layout";
-import Login from "./components/Login";
-import MyFiles from "./components/MyFiles";
-import { Authenticated, Roles } from "./authentication";
-import {Registration} from "./components/Registration/Registration.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./App.css"
+import { Authenticated, Roles, useRefreshSession } from "./authentication"
+import Dashboard from "./components/Dashboard"
+import { Layout } from "./components/Layout/Layout"
+import Login from "./components/Login"
+import MyFiles from "./components/MyFiles"
+import { Registration } from "./components/Registration/Registration.tsx"
 
-const approvedRoles = [Roles.ORGANISATION, Roles.TREY_BOARD, Roles.ADMIN];
+const approvedRoles = [Roles.ORGANISATION, Roles.TREY_BOARD, Roles.ADMIN]
 
-const App = ()=> {
-    return <BrowserRouter>
-            <Routes>
+const App = () => {
+  useRefreshSession()
 
-                <Route path="/dashboard" element={<Authenticated requiredRoles={approvedRoles} redirectUrl={"/registration"}>
-                        <Layout>
-                            <Dashboard/>
-                        </Layout>
-                    </Authenticated>}/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <Authenticated requiredRoles={approvedRoles} redirectUrl={"/registration"}>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Authenticated>
+          }
+        />
 
-                <Route path="/my-files" element={<Authenticated requiredRoles={approvedRoles} redirectUrl={"/registration"}>
-                        <Layout>
-                            <MyFiles/>
-                        </Layout>
-                    </Authenticated>}/>
+        <Route
+          path="/my-files"
+          element={
+            <Authenticated requiredRoles={approvedRoles} redirectUrl={"/registration"}>
+              <Layout>
+                <MyFiles />
+              </Layout>
+            </Authenticated>
+          }
+        />
 
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
 
-                <Route path="registration" element={
-                    <Authenticated requiredRoles={[Roles.NONE]} redirectUrl={"/registration"} >
-                        <Layout>
-                            <Registration />
-                        </Layout>
-                    </Authenticated>}>
-                </Route>
-            </Routes>
+        <Route
+          path="registration"
+          element={
+            <Authenticated requiredRoles={[Roles.NONE]} redirectUrl={"/registration"}>
+              <Layout>
+                <Registration />
+              </Layout>
+            </Authenticated>
+          }
+        ></Route>
+      </Routes>
     </BrowserRouter>
+  )
 }
 
 export default App
