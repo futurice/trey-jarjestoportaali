60790fde2562391298f615ac446b965b663bf4bc
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-12 15:06:20 +0200

    chore: Remove radix-ui from other components

--- src/web/src/components/Header/Header.tsx
+++ src/web/src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
 import React from "react"
 import { AppBar } from "@mui/material"
-import { Container } from "@radix-ui/themes"
 import Navigation from "../Navigation/Navigation"
 
 export const Header: React.FC = () => {
   return (
     <AppBar position="static" sx={{ padding: "2px 15px 2px 15px" }}>
-      <Container maxWidth="xl">
-        <Navigation />
-      </Container>
+      <Navigation />
     </AppBar>
   )
 }
--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { createTheme, ThemeProvider } from "@mui/material"
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material"
 import { StytchProvider } from "@stytch/react"
 import { StytchUIClient } from "@stytch/vanilla-js"
-import { Theme } from "@radix-ui/themes"
-import "@radix-ui/themes/styles.css"
 import App from "./App.tsx"
 import "./index.css"
 
 const stytch = new StytchUIClient("public-token-test-37e5d883-b15b-45cc-8615-a355d9bc27af")
 
 const theme = createTheme({
+  cssVariables: true,
   palette: {
     primary: {
       main: "#006069",
@@ -23,11 +22,10 @@ const theme = createTheme({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <StytchProvider stytch={stytch}>
       <ThemeProvider theme={theme}>
-        <Theme accentColor="blue" hasBackground={false} panelBackground="solid">
-          <App />
-        </Theme>
+        <App />
       </ThemeProvider>
     </StytchProvider>
   </StrictMode>,
