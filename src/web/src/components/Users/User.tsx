d0da6aae0302a436826cb0681d0afb5e30718712
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-19 12:57:08 +0200

    feat: Add first version of user edit modal

--- src/web/src/components/Users/User.tsx
+++ src/web/src/components/Users/User.tsx
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react"
 import { NavLink, useParams } from "react-router-dom"
-import { ArrowLeftIcon } from "@radix-ui/react-icons"
+import {
+  Button,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  DialogActions,
+  Dialog,
+  InputLabel,
+  FormControl,
+  MenuItem,
+  Select,
+  TextField,
+  Box,
+} from "@mui/material"
+import { ArrowLeftIcon, Pencil1Icon } from "@radix-ui/react-icons"
 import { Container, DataList } from "@radix-ui/themes"
 import { getUser } from "../../api/users"
+import { Roles } from "../../authentication"
 import { User } from "../../models/user"
 import styles from "./User.module.css"
 
@@ -21,10 +36,69 @@ const UserInfo = ({ user }: { user: User }) => {
   )
 }
 
+const UserModal = ({
+  open,
+  close,
+  user,
+}: {
+  open: boolean
+  close: () => void
+  user: User | null
+}) => {
+  return user ? (
+    <Dialog open={open} onClose={close} sx={{ display: "flex", justifyContent: "center" }}>
+      <DialogTitle>User details</DialogTitle>
+      <DialogContent>
+        <Box sx={{ p: 1, display: "flex", flexDirection: "column", gap: 2, minWidth: "500px" }}>
+          <FormControl>
+            <TextField
+              label="User ID"
+              defaultValue={user.userId}
+              sx={{ width: "100%" }}
+              placeholder="User ID"
+              disabled
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              label="Email"
+              defaultValue={user.emails[0].email}
+              sx={{ width: "100%" }}
+              placeholder="Email"
+            />
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Role</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={user.trustedMetadata?.role}
+              label="Role"
+              onChange={(e) => console.log(e)}
+            >
+              {Object.values(Roles).map((role) => (
+                <MenuItem key={role} value={role}>
+                  {role}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={close}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  ) : (
+    <></>
+  )
+}
+
 const UserPage = () => {
   const { userId } = useParams()
-  console.log(userId)
   const [user, setUser] = useState<User | null>(null)
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     if (!userId) {
@@ -41,7 +115,16 @@ const UserPage = () => {
         <ArrowLeftIcon width="32" height="32" />
       </NavLink>
       <h2>{user?.emails[0].email}</h2>
+      <Button
+        variant="contained"
+        onClick={() => setOpen(true)}
+        sx={{ mb: 2, display: "flex", alignItems: "center", gap: 1 }}
+      >
+        <Pencil1Icon />
+        <Typography>Edit</Typography>
+      </Button>
       <UserInfo user={user} />
+      <UserModal open={open} close={() => setOpen(false)} user={user} />
     </Container>
   ) : (
     <Container>Loading...</Container>
