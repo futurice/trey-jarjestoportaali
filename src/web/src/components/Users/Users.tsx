02e509fa73be5b8f7d98efbd91b17bc4af02d7e3
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-19 12:56:43 +0200

    feat: Change Users page to use Material-UI components

--- src/web/src/components/Users/Users.tsx
+++ src/web/src/components/Users/Users.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import { Badge, Container, Table } from "@radix-ui/themes"
+import AccountBoxIcon from "@mui/icons-material/AccountBox"
+import { Chip, Container, IconButton } from "@mui/material"
+import { DataGrid, GridColDef } from "@mui/x-data-grid"
 import { getUsers } from "../../api/users"
 import { Roles } from "../../authentication"
 import { User } from "../../models/user"
@@ -8,42 +10,28 @@ import { User } from "../../models/user"
 const UserBadge = ({ role }: { role: Roles | undefined }) => {
   switch (role) {
     case Roles.ADMIN:
-      return <Badge color="orange">Admin</Badge>
+      return <Chip size="small" sx={{ backgroundColor: "#db6e00", color: "white" }} label="Admin" />
     case Roles.TREY_BOARD:
-      return <Badge color="blue">TREY</Badge>
+      return <Chip size="small" sx={{ backgroundColor: "#006069", color: "white" }} label="TREY" />
     case Roles.ORGANISATION:
-      return <Badge color="green">Organisation</Badge>
+      return (
+        <Chip
+          size="small"
+          sx={{ backgroundColor: "#009c15", color: "white" }}
+          label="Organisation"
+        />
+      )
     case Roles.PENDING_APPROVAL:
-      return <Badge color="red">Pending Approval</Badge>
+      return (
+        <Chip
+          size="small"
+          sx={{ backgroundColor: "#9c0005", color: "white" }}
+          label="Pending approval"
+        />
+      )
     default:
-      return <Badge color="gray">Unknown</Badge>
+      return <Chip size="small" color="default" label="Unknown" />
   }
-}
-
-const UsersTable = ({ users }: { users: User[] }) => {
-  return (
-    <Container>
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Role</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {users.map((user: User) => (
-            <Table.Row key={user.userId}>
-              <Table.Cell>{<UserBadge role={user.trustedMetadata?.role} />}</Table.Cell>
-              <Table.Cell>
-                <Link to={user.userId}>{user.emails[0].email}</Link>{" "}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </Container>
-  )
 }
 
 const Users = () => {
@@ -54,10 +42,51 @@ const Users = () => {
     })
   }, [])
 
+  const columns: GridColDef<User>[] = [
+    {
+      field: "edit",
+      headerName: "Edit",
+      width: 80,
+      sortable: false,
+      renderCell: (params) => (
+        <Link to={`/users/${params.row.userId}`}>
+          <IconButton size="small">
+            <AccountBoxIcon />
+          </IconButton>
+        </Link>
+      ),
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      width: 130,
+      valueGetter: (_value, row) => row.trustedMetadata?.role,
+      renderCell: (params) => <UserBadge role={params.value} />,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 300,
+      valueGetter: (_value, row) => row.emails[0].email,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      sortable: false,
+      width: 160,
+      valueGetter: (_value, row) => `${row.name?.firstName || ""} ${row.name?.lastName || ""}`,
+    },
+  ]
+
   return (
-    <Container>
+    <Container sx={{ p: 1 }}>
       <h1>Users</h1>
-      <UsersTable users={users} />
+      <DataGrid
+        rows={users}
+        columns={columns}
+        getRowId={(row) => row.userId}
+        disableRowSelectionOnClick
+      />
     </Container>
   )
 }
