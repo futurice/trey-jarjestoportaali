a4b01d84ea2d63efc6ea93a461f059efca015f52
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-21 13:14:05 +0200

    feat: Initialize i18next for project

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import { StytchProvider } from "@stytch/react"
 import { StytchUIClient } from "@stytch/vanilla-js"
 import App from "./App.tsx"
+import "./i18n"
 import "./index.css"
 
 const stytch = new StytchUIClient("public-token-test-37e5d883-b15b-45cc-8615-a355d9bc27af")
