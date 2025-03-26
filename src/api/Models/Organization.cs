e7965da500f7d3c8ec6fab17a74c80f3ceff9af6
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-03-26 14:33:17 +0200

    fix: Add missing Organization object attributes

--- src/api/Models/Organization.cs
+++ src/api/Models/Organization.cs
@@ -1,15 +1,26 @@
 namespace Trey.Api.Models;
 
+public enum Category
+{
+  FacultyAndUmbrella,
+  Hobby,
+  StudentAssociation,
+  Other
+}
+
 public class Person
 {
   public required string Name { get; set; }
   public string? Email { get; set; }
   public string? Phone { get; set; }
   public string? TelegramNick { get; set; }
+  public bool? HasBankingAccount { get; set; }
 }
 
 public class Facility
 {
+  public string? Campus { get; set; }
+  public string? Building { get; set; }
   public string? RoomCode { get; set; }
   public Person? ContactPerson { get; set; }
   public string? OtherInfo { get; set; }
@@ -24,6 +35,8 @@ public class TimeRange
 public class Organization
 {
   public Guid Id { get; set; } = Guid.NewGuid();
+  public Category? Category { get; set; }
+  public string? Website { get; set; }
   public required string Name { get; set; }
   public string? ShortName { get; set; }
   public int? FoundingYear { get; set; }
@@ -31,10 +44,12 @@ public class Organization
   public required string Email { get; set; }
   public required Person Chairperson { get; set; }
   public List<Person>? Boardmembers { get; set; }
+  public List<Person>? SignatureRightsOwners { get; set; }
+  public Person? IntraRightsOwner { get; set; }
   public int MemberCount { get; set; }
   public int? TreyMemberCount { get; set; }
   public string? IBAN { get; set; }
   public List<string>? ReservationRightsEmails { get; set; }
-  public Dictionary<string, string>? EmailLists { get; set; }
+  public Dictionary<string, string[]>? EmailLists { get; set; }
   public Facility? AssociationFacility { get; set; }
 }
