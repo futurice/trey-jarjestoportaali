1e0292ec6b4a78e3275ae795debc6ca35feb6347
Rudolf <rudolf.poels75@gmail.com>
2025-03-29 19:01:16 +0200

    fix: context requires scope

--- src/api/Middleware/RoleAuthorizationMiddleware.cs
+++ src/api/Middleware/RoleAuthorizationMiddleware.cs
@@ -3,8 +3,14 @@ using Trey.Api.Services;
 
 namespace Trey.Api.Middleware;
 
-public class RoleAuthorizationMiddleware(RequestDelegate next, IAuthService authService)
+public class RoleAuthorizationMiddleware(RequestDelegate next, IServiceScopeFactory serviceScopeFactory)
 {
+    public static RequestDelegate Create(RequestDelegate next, IServiceScopeFactory serviceScopeFactory)
+    {
+        var middleware = new RoleAuthorizationMiddleware(next, serviceScopeFactory);
+        return middleware.InvokeAsync;
+    }
+
     public async Task InvokeAsync(HttpContext context)
     {
         var endpoint = context.GetEndpoint();
@@ -23,11 +29,15 @@ public class RoleAuthorizationMiddleware(RequestDelegate next, IAuthService auth
 
         try
         {
-            var user = await authService.GetUserFromContext(context);
-            if (user.Role != requiredRoleAttribute.Role)
+            using (var scope = serviceScopeFactory.CreateScope())
             {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                return;
+                var authService = scope.ServiceProvider.GetRequiredService<IAuthService>();
+                var user = await authService.GetUserFromContext(context);
+                if (user.Role != requiredRoleAttribute.Role)
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return;
+                }
             }
 
             await next(context);
--- src/api/Models/RequiredRoleAttribute.cs
+++ src/api/Models/RequiredRoleAttribute.cs
@@ -1,5 +1,3 @@
-using System.Security.Claims;
-
 namespace Trey.Api.Models;
 
 [AttributeUsage(AttributeTargets.Method)]
