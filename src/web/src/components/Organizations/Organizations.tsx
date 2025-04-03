d5395e39aceda281b209db6271c88c0e207efd9c
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-04-03 12:16:14 +0300

    feat: add i18n and filtering for Organizations

--- src/web/src/components/Organizations/Organizations.tsx
+++ src/web/src/components/Organizations/Organizations.tsx
@@ -1,14 +1,15 @@
 import { useMemo } from "react"
 import { useTranslation } from "react-i18next"
 import { Container } from "@mui/material"
-import { DataGrid, GridColDef } from "@mui/x-data-grid"
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid"
+import { fiFI, enUS } from "@mui/x-data-grid/locales"
 import { useStytch } from "@stytch/react"
 import { useAuth } from "../../hooks/useAuth"
 import { useOrganizationService } from "../../hooks/useOrganizationService"
 import { useOrganizations } from "../../hooks/useOrganizations"
 
 export const Organizations: React.FC = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const { user } = useAuth()
   const { session } = useStytch()
   const sessionJwt = useMemo(() => session?.getTokens()?.session_jwt, [session])
@@ -17,20 +18,32 @@ export const Organizations: React.FC = () => {
   const { organizations, loading } = useOrganizations(organizationService)
 
   const columns: GridColDef[] = [
-    { field: "name", headerName: t("organization.name"), width: 300 },
-    { field: "email", headerName: t("organization.email"), width: 200 },
+    { field: "name", headerName: t("name"), width: 400 },
+    { field: "email", headerName: t("email"), width: 300 },
   ]
 
   return (
     <Container>
-      <h1>{t("organizations")}</h1>
+      <h1>{t("navigation.organizations")}</h1>
       <DataGrid
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+        localeText={
+          (i18n.language === "fi" ? fiFI : enUS).components.MuiDataGrid.defaultProps.localeText
+        }
         rows={organizations}
         columns={columns}
         initialState={{
           pagination: { paginationModel: { pageSize: 20 } },
         }}
         pageSizeOptions={[20, 50, 75, 100, { value: -1, label: t("common.all") }]}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+          },
+        }}
         autosizeOnMount
         loading={loading}
       />
