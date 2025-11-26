947abf0ef7edecb230271798f5f8b22ea9a6c0f2
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-11-26 16:39:12 +0200

    feat: Add organization fetching endpoints

---
+++ src/web/src/hooks/useOrganizations.ts
@@ -0,0 +1,84 @@
+import { useState, useEffect, useCallback } from "react"
+import { User } from "@supabase/supabase-js"
+import { useQuery } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+import { Organization } from "../models/organization"
+import { OrganizationService } from "../services/organizationService"
+
+export const useOrganizations = (organizationsService: OrganizationService | null) => {
+  const [organizations, setOrganizations] = useState<Organization[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchOrganizations = useCallback(async () => {
+    if (!organizationsService) {
+      setOrganizations([])
+      setLoading(false)
+      return
+    }
+
+    try {
+      const organizationList = await organizationsService.getList()
+      setOrganizations(organizationList)
+    } catch {
+      setOrganizations([])
+    } finally {
+      setLoading(false)
+    }
+  }, [organizationsService])
+
+  useEffect(() => {
+    setLoading(true)
+    fetchOrganizations()
+  }, [fetchOrganizations])
+
+  return { organizations, loading }
+}
+
+export const useGetOrganizationByUser = (
+  organizationsService: OrganizationService | null,
+  user: User | null,
+) => {
+  const [organization, setOrganization] = useState<Organization | null>(null)
+  const [loading, setLoading] = useState(true)
+  const fetchOrganization = useCallback(async () => {
+    if (!organizationsService || !user) {
+      setOrganization(null)
+      setLoading(false)
+      return
+    }
+
+    try {
+      const userOrganizationId = user.user_metadata?.organization_id
+      if (!userOrganizationId) {
+        setOrganization(null)
+      } else {
+        const org = await organizationsService.get(userOrganizationId)
+        setOrganization(org)
+      }
+    } catch {
+      setOrganization(null)
+    } finally {
+      setLoading(false)
+    }
+  }, [organizationsService, user])
+
+  useEffect(() => {
+    setLoading(true)
+    fetchOrganization()
+  }, [fetchOrganization])
+
+  return { organization, loading }
+}
+
+export const useGetOrganizationById = (
+  organizationsService: OrganizationService | null,
+  organizationId: string | undefined,
+) => {
+  return useQuery<Organization, AxiosError>({
+    queryKey: ["organization", organizationId],
+    queryFn: async () => {
+      return await organizationsService!.get(organizationId!)
+    },
+    enabled: !!organizationId || !!organizationsService,
+  })
+}
