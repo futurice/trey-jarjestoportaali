2fc60e3dd0256ef2922ae81efe5d1eb4e93273cd
Kiti Suupohja <kiti.suupohja@futurice.com>
2025-12-16 15:33:53 +0200

    cleanup

--- src/api/Services/EmailService.cs
+++ src/api/Services/EmailService.cs
@@ -10,24 +10,22 @@ public interface IEmailService
 
 internal sealed class EmailService : IEmailService
 {
-  private readonly Supabase.Client supabaseClient;
   private readonly EmailClient emailClient;
 
-  public EmailService(Supabase.Client supabaseClient, EmailClient emailClient)
+  public EmailService(EmailClient emailClient)
   {
-    this.supabaseClient = supabaseClient;
     this.emailClient = emailClient;
   }
 
   public async Task<EmailSendOperation> SendAccountCreatedEmail(string email, string username, string password)
   {
-    var emailContent = new EmailContent("TREY Jippo - Uusi käyttäjätili luotu")
+    var emailContent = new EmailContent("TREY Jippo - Uusi käyttäjätili luotu / New user account created")
     {
-      Html = "<html><body><div class=\"email-fi\"><h1>TREY Jippo - Kirjautuminen on uudistunut!</h1><p>Jippoon kirjautuminen on uudistunut, ja kaikille TREY:n järjestöille on luotu uudet kirjautumistiedot.</p><p>Voit kirjautua Jippoon osoitteessa <a href=\"https://jippo.trey.fi\">https://jippo.trey.fi</a></p><ul><li><strong>Käyttäjätunnus:</strong> {{Username}}</li><li><strong>Salasana:</strong> {{Password}}</li></ul><p>Suosittelemme vaihtamaan generoidun salasanan mahdollisimman pian.</p></div><div class=\"email-en\"><h1>TREY Jippo - Login flow has been renewed!</h1><p>The login flow for Jippo has been renewed, and new login credentials have been created for all TREY organizations.</p><p>You can log in to Jippo at <a href=\"https://jippo.trey.fi\">https://jippo.trey.fi</a></p><ul><li><strong>Username:</strong> {{Username}}</li><li><strong>Password:</strong> {{Password}}</li></ul><p>We recommend changing the generated password as soon as possible.</p></div></body></html>"
+      Html = $"<html><body><div class=\"email-fi\"><h1>TREY Jippo - Kirjautuminen on uudistunut!</h1><p>Jippoon kirjautuminen on uudistunut, ja kaikille TREY:n järjestöille on luotu uudet kirjautumistiedot.</p><p>Voit kirjautua Jippoon osoitteessa <a href=\"https://jippo.trey.fi\">https://jippo.trey.fi</a></p><ul><li><strong>Käyttäjätunnus:</strong> {username}</li><li><strong>Salasana:</strong> {password}</li></ul><p>Suosittelemme vaihtamaan generoidun salasanan mahdollisimman pian.</p></div><div class=\"email-en\"><h1>TREY Jippo - Login flow has been renewed!</h1><p>The login flow for Jippo has been renewed, and new login credentials have been created for all TREY organizations.</p><p>You can log in to Jippo at <a href=\"https://jippo.trey.fi\">https://jippo.trey.fi</a></p><ul><li><strong>Username:</strong> {username}</li><li><strong>Password:</strong> {password}</li></ul><p>We recommend changing the generated password as soon as possible.</p></div></body></html>"
     };
 
     var emailMessage = new EmailMessage(
-        senderAddress: "trey-jippo@jippo.trey.fi",
+        senderAddress: "trey-jippo@5e57e8ca-4d68-4fe1-99da-9e818a907d84.azurecomm.net",
         recipientAddress: email,
         content: emailContent
     );
