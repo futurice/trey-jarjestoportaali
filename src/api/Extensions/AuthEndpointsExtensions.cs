8fea3190ce89b05d59e79ef6c8f971c12a91a7cf
Kiti Suupohja <kiti.suupohja@futurice.com>
2026-01-08 11:34:44 +0200

    fix: Throw unauthorized error if username or password is incorrect (#50)
    * fix: Throw unauthorized error if username or password is incorrect
    
    * fix: Change logging to ILogger
    
    * fix: Fix exception catching logic
    
    * fix: Change logger messages

--- src/api/Extensions/AuthEndpointsExtensions.cs
+++ src/api/Extensions/AuthEndpointsExtensions.cs
@@ -1,7 +1,5 @@
-using System.ComponentModel.DataAnnotations;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using Supabase.Gotrue.Exceptions;
 using Trey.Api.Models;
 using Trey.Api.Services;
@@ -18,70 +16,34 @@ public static class AuthEndpointsExtensions
     return group;
   }
 
-  private static async Task<IResult> AuthorizeUser(Supabase.Client supabase, [FromBody] LoginData loginData)
+  private static async Task<IResult> AuthorizeUser([FromServices] IAuthService authService, [FromServices] ILogger<AuthService> _logger, [FromBody] LoginData loginData)
   {
-    var email = new EmailAddressAttribute();
-    if (email.IsValid(loginData.Username))
-    {
-      try
-      {
-        var emailResponse = await supabase.Auth.SignInWithPassword(loginData.Username, loginData.Password);
-        if (emailResponse?.User == null)
-        {
-          return TypedResults.Unauthorized();
-        }
-        return TypedResults.Ok(emailResponse);
-      }
-      catch (GotrueException ex)
-      {
-        // Log the exception if logging is available
-        if (ex.StatusCode == 400 || ex.StatusCode == 401)
-        {
-          return TypedResults.Unauthorized();
-        }
-        return TypedResults.Problem("An error occurred while attempting to sign in with email.", statusCode: (int)HttpStatusCode.InternalServerError);
-      }
-
-    }
-    // The 'email_for_username' RPC function retrieves the email address associated with the given username from the database.
-    // This is necessary for the username-based login flow, as Supabase authentication requires an email address.
-    // By mapping the provided username to its corresponding email, we enable users to log in using their username.
-    string? userEmail = null;
-    Supabase.Postgrest.Responses.BaseResponse? userEmailResponse = null;
     try
     {
-      userEmailResponse = await supabase.Rpc("email_for_username", new { p_username = loginData.Username });
-      userEmail = JsonConvert.DeserializeObject<string>(userEmailResponse?.Content?.ToString() ?? "");
+      var sessionResponse = await authService.LoginUser(loginData.Username, loginData.Password);
+      if (sessionResponse?.User == null) return TypedResults.Unauthorized();
+
+      return TypedResults.Ok(sessionResponse);
+    }
+    catch (Exception ex) when ((ex is UnauthorizedAccessException) || (ex is GotrueException gEx && (gEx.StatusCode == 400 || gEx.StatusCode == 401)))
+    {
+      return TypedResults.Unauthorized();
     }
     catch (Exception ex)
     {
-      // Log the exception if logging is available
-      return TypedResults.Problem("An error occurred while retrieving the user email for the provided username.", statusCode: (int)HttpStatusCode.InternalServerError);
+      _logger.LogError(ex, $"Exception while signing in user {loginData.Username}");
+      return TypedResults.Problem("An error occurred while attempting to sign in.", statusCode: (int)HttpStatusCode.InternalServerError);
     }
-    if (userEmailResponse?.Content == null || userEmailResponse.ResponseMessage?.StatusCode == HttpStatusCode.NotFound)
-    {
-      return TypedResults.Unauthorized();
-    }
-    else if (userEmail == null || !email.IsValid(userEmail))
-    {
-      return TypedResults.Unauthorized();
-    }
-    var response = await supabase.Auth.SignInWithPassword(userEmail, loginData.Password);
-    if (response?.User == null)
-    {
-      return TypedResults.Unauthorized();
-    }
-    return TypedResults.Ok(response);
   }
 
-  private static async Task<IResult> CreateUser([FromServices] IAuthService authService, [FromServices] ILogger<FileService> logger, [FromBody] CreateTreyUserRequest createUserData, HttpContext context)
+  private static async Task<IResult> CreateUser([FromServices] IAuthService authService, [FromServices] ILogger<AuthService> _logger, [FromBody] CreateTreyUserRequest createUserData, HttpContext context)
   {
     try
     {
       var user = await authService.GetUserFromContext(context);
       if (user.Role != TreyRole.Admin)
       {
-        logger.LogWarning("User {userId} is not authorized to create users", user.Id);
+        _logger.LogWarning("User {userId} is not authorized to create users", user.Id);
         return TypedResults.Forbid();
       }
       var createdUser = await authService.CreateUser(createUserData);
@@ -89,19 +51,18 @@ public static class AuthEndpointsExtensions
     }
     catch (Exception ex)
     {
-      // Log the exception if logging is available
-      Console.WriteLine(ex.Message);
+      _logger.LogError(ex, "Error occurred while creating the user.");
       return TypedResults.Problem("An error occurred while creating the user.", statusCode: (int)HttpStatusCode.InternalServerError);
     }
   }
-  private static async Task<IResult> CreateMultipleUsers([FromServices] IAuthService authService, [FromServices] ILogger<FileService> logger, [FromBody] CreateTreyUserRequest[] createUserData, HttpContext context)
+  private static async Task<IResult> CreateMultipleUsers([FromServices] IAuthService authService, [FromServices] ILogger<AuthService> _logger, [FromBody] CreateTreyUserRequest[] createUserData, HttpContext context)
   {
     try
     {
       var user = await authService.GetUserFromContext(context);
       if (user.Role != TreyRole.Admin)
       {
-        logger.LogWarning("User {userId} is not authorized to create users", user.Id);
+        _logger.LogWarning("User {userId} is not authorized to create users", user.Id);
         return TypedResults.Forbid();
       }
       var users = await authService.CreateMultipleUsers(createUserData);
@@ -110,7 +71,7 @@ public static class AuthEndpointsExtensions
     catch (Exception ex)
     {
       // Log the exception if logging is available
-      Console.WriteLine(ex.Message);
+      _logger.LogError(ex, "Error occurred while creating multiple users.");
       return TypedResults.Problem("An error occurred while creating the users.", statusCode: (int)HttpStatusCode.InternalServerError);
     }
   }
