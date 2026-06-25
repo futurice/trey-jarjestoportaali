fad3815dcb93129b6156e9e3805981fde2518ed8
Kiti Suupohja <kiti.suupohja@futurice.com>
2026-06-25 13:51:25 +0300

    chore: Use Sentry only in production env

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -11,31 +11,33 @@ import config from "./config/index.ts"
 import "./i18n"
 import "./index.css"
 
-Sentry.init({
-  dsn: config.sentry.dsn,
-  integrations: [
-    Sentry.feedbackAsyncIntegration({
-      showBranding: false,
-      colorScheme: "system",
-      triggerLabel: t("feedback.trigger"),
-      cancelButtonLabel: t("feedback.cancel"),
-      confirmButtonLabel: t("feedback.confirm"),
-      addScreenshotButtonLabel: t("feedback.addScreenshot"),
-      removeScreenshotButtonLabel: t("feedback.removeScreenshot"),
-      nameLabel: t("feedback.name"),
-      namePlaceholder: t("feedback.namePlaceholder"),
-      emailLabel: t("feedback.email"),
-      emailPlaceholder: t("feedback.emailPlaceholder"),
-      isRequiredLabel: t("feedback.isRequired"),
-      messageLabel: t("feedback.message"),
-      messagePlaceholder: t("feedback.messagePlaceholder"),
-      successMessageText: t("feedback.successMessage"),
-      buttonLabel: t("feedback.label"),
-      submitButtonLabel: t("feedback.submit"),
-      formTitle: t("feedback.title"),
-    }),
-  ],
-})
+if (config.env !== "dev") {
+  Sentry.init({
+    dsn: config.sentry.dsn,
+    integrations: [
+      Sentry.feedbackAsyncIntegration({
+        showBranding: false,
+        colorScheme: "system",
+        triggerLabel: t("feedback.trigger"),
+        cancelButtonLabel: t("feedback.cancel"),
+        confirmButtonLabel: t("feedback.confirm"),
+        addScreenshotButtonLabel: t("feedback.addScreenshot"),
+        removeScreenshotButtonLabel: t("feedback.removeScreenshot"),
+        nameLabel: t("feedback.name"),
+        namePlaceholder: t("feedback.namePlaceholder"),
+        emailLabel: t("feedback.email"),
+        emailPlaceholder: t("feedback.emailPlaceholder"),
+        isRequiredLabel: t("feedback.isRequired"),
+        messageLabel: t("feedback.message"),
+        messagePlaceholder: t("feedback.messagePlaceholder"),
+        successMessageText: t("feedback.successMessage"),
+        buttonLabel: t("feedback.label"),
+        submitButtonLabel: t("feedback.submit"),
+        formTitle: t("feedback.title"),
+      }),
+    ],
+  })
+}
 
 const theme = createTheme({
   cssVariables: true,
